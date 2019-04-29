8503cdf128a3a044176964013aaaffc614e3be50
Oleksii.Petrychenko <37137346+petrychenkodev@users.noreply.github.com>
2019-04-29 10:40:35 +0300

    Add files via upload

---
+++ precache-manifest.a0eca250fb77c5dc5afeebb8d10a55b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51516e833ec13ef3cf79ad7f1fe7ca7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "442b7518705fac2762fa",
+    "url": "/static/css/main.3fb8b239.chunk.css"
+  },
+  {
+    "revision": "35c42171b55e990c4735",
+    "url": "/static/js/2.09bbba05.chunk.js"
+  },
+  {
+    "revision": "442b7518705fac2762fa",
+    "url": "/static/js/main.c488e48e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
